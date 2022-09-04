cfdfaf12ace43a295a06c135405a55d114bf2c03
hkeremk <hkeremk@users.noreply.github.com>
2022-09-04 10:47:01 +0300

    test.js fixed

--- tests.js
+++ tests.js
@@ -3,7 +3,7 @@ const expect = chai.expect
 
 describe("getCelcius", () => {
     it("Should convert fahrenheit to celcius for all values in array", () => {
-        expect(getCelcius([23, 140, 212, 41])).to.deep.equal([-5, 30, 100, 5])
+        expect(getCelcius([23, 140, 212, 41])).to.deep.equal([-5, 60, 100, 5])
     })
 })
 
